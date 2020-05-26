58c664f84dffe0ecc037200283c9dac5d2d013cb
pugazh.siva1 <sivasiv@gmail.com>
2020-05-26 20:13:44 +0530

    First commit

--- Basic3.py
+++ Basic3.py
@@ -3,4 +3,18 @@ import tensorflow as tf
 graph3 =  tf.Graph()
 
 with graph3.as_default():
+
     Matrix_one = tf.constant([[1, 2, 3], [2, 3, 4], [3, 4, 5]])
+    Matrix_two = tf.constant([[2, 2, 2], [2, 2, 2], [2, 2, 2]])
+
+    add_1_operation = tf.add(Matrix_one, Matrix_two)
+    add_2_operation = Matrix_one + Matrix_two
+
+with tf.compat.v1.Session(graph =graph3) as sess:
+
+    result = sess.run(add_1_operation)
+    print ("Defined using tensorflow function :")
+    print(result)
+    result = sess.run(add_2_operation)
+    print ("Defined using normal expressions :")
+    print(result)
