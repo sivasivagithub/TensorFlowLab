bf9a1497d92bb0b35d78776ef4f0e904a66196af
pugazh.siva1 <sivasiv@gmail.com>
2020-06-06 20:58:32 +0530

    First commit

---
+++ Exercise_1_House_Prices_Question_neu_net.py
@@ -0,0 +1,29 @@
+
+#In this exercise you'll try to build a neural network that predicts the price of a house according to a simple formula.
+#So, imagine if house pricing was as easy as a house costs 50k + 50k per bedroom, so that a 1 bedroom house costs 100k, a 2 bedroom house costs 150k etc.
+#How would you create a neural network that learns this relationship so that it would predict a 7 bedroom house as costing close to 400k etc.
+#Hint: Your network might work better if you scale the house price down. You don't have to give the answer 400...it might be better to create something
+# that predicts the number 4, and then your answer is in the 'hundreds of thousands' etc.
+
+# 7*50 + 50 = 350 + 50 = 400/100
+# y = x + 1  [1, 2, 3, 4]
+# [1, 1.5, 2, 2.5]
+
+import tensorflow as tf
+import numpy as np
+from tensorflow import keras
+
+# GRADED FUNCTION: house_model
+
+def house_model(y_new):
+    xs = np.array([1.0, 2.0, 3.0, 4.0], dtype=float)
+    ys = np.array([1.0, 1.5, 2.0, 2.5], dtype=float)
+    model = tf.keras.Sequential([keras.layers.Dense(units=1, input_shape =[1] )])  #Define a neural network
+    model.compile(optimizer= 'sgd', loss='mean_squared_error')
+    model.fit(xs , ys, epochs=500)
+
+    return model.predict(y_new)[0]
+
+prediction = house_model([7.0])
+print(prediction)
+
